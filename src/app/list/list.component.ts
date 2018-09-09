26ea4dab4ed0f786f9cb0bd86f83d6d565e21a4f
Guilherme <guilhermemelo6@outlook.com>
2018-09-09 18:54:20 -0300

    Created a mock list

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Task } from '../task';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-  task = {title:'Titulo da atividade', description: 'Detalhes desta atividade descrita'}
+  task: Task = {id: 1,title:'Titulo da atividade', description: 'Detalhes desta atividade descrita', completed: false};
   constructor() { }
 
   ngOnInit() {
---
+++ src/app/mock-list.ts
@@ -0,0 +1,39 @@
+import { Task } from './task';
+
+export const TASKS: Task[] = [
+  {
+    id:1,
+    title: 'A',
+    completed: false
+  },
+  {
+    id: 2,
+    title: 'Fazer aquilo la',
+    description: 'Bem ali',
+    completed: true
+  },{
+    id: 3,
+    title: 'Ir ali',
+    description: 'Fazer aquilo la',
+    completed: false
+  },
+  {
+    id: 4,
+    title: 'Banhar Sephiroth',
+    description: 'É bom dar atenção às patas, tão muito sujas',
+    completed: true
+  },
+  {
+    id: 5,
+    title: 'Calibrar os pneus do carro',
+    completed: false
+  },
+  {
+    id: 6,
+    title: 'Comprar comida',
+    description: 'É bom comprar cedo, já que não vai dar tempo se deixar pra muito tarde',
+    completed: false
+  }
+
+
+];
