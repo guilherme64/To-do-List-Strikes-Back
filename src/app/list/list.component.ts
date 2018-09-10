3d9754e2a6a830668dc081068c32209e76a52755
Guilherme <guilhermemelo6@outlook.com>
2018-09-10 00:23:44 -0300

    Trying to use firebase without the service

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Task } from '../task';
 import { TASKS } from '../mock-list';
+import { TaskService } from '../task.service';
+import { Observable } from 'rxjs';
+import { AngularFireDatabase } from 'angularfire2/database';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-  task: Task = {id: 1,title:'Titulo da atividade', description: 'Detalhes desta atividade descrita', completed: false};
-  tasks = TASKS;
+  task: Task = new Task;
+  tasks: any = TASKS;
   editMode = false;
-  constructor() { }
+  constructor(public db: AngularFireDatabase) { }
 
   ngOnInit() {
+    this.getTasks();
   }
   onSelect(task: Task){
     console.log(task.id);
@@ -28,6 +32,19 @@ export class ListComponent implements OnInit {
   onSubmitEdit(task: Task){
     console.log(task.id +' Sera editada');
     this.editMode = false;
+  }
+
+  onAddTask(task: Task){
+    //this.task.title = task.title;
+    
+    this.task.completed = false;
+    console.log('tentando adicionar');
+    console.log(this.task.title);
+    this.taskService.addTask(this.task);
+  }
+
+  getTasks() {
+    this.tasks = this.db.list('tasks').valueChanges();
   }
 
 }
--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Task } from './task';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private dbPath = '/tasks';
+  tasks: AngularFireList<Task> = null;
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase) { 
+    this.tasks = db.list(this.dbPath);
+  }
+   addTask(task: Task): void {
+     console.log('tentando mandar pro firebase');
+    this.tasks.push(task);
+  }
+  getTasks(): AngularFireList<Task>{
+    //return 
+  }
+ 
 }
