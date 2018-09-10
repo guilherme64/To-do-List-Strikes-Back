502104c36a14d80de72fdd041cf4828d58d920ce
Guilherme <guilhermemelo6@outlook.com>
2018-09-10 01:01:35 -0300

    Goofing around with some firebase functions

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -10,13 +10,16 @@ import { AngularFireDatabase } from 'angularfire2/database';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-  task: Task = new Task;
-  tasks: any = TASKS;
+  task: Task = new Task();
+  tasks: Observable<any>;
+
   editMode = false;
-  constructor(public db: AngularFireDatabase) { }
+  constructor(public db: AngularFireDatabase) { 
+    this.tasks = db.list('tasks').valueChanges();
+  }
 
   ngOnInit() {
-    this.getTasks();
+    
   }
   onSelect(task: Task){
     console.log(task.id);
@@ -36,15 +39,24 @@ export class ListComponent implements OnInit {
 
   onAddTask(task: Task){
     //this.task.title = task.title;
-    
-    this.task.completed = false;
+    this.db.list('/tasks').push({title: this.task.title, 
+    description: this.task.description, completed: false});
+    //this.task.completed = false;
     console.log('tentando adicionar');
-    console.log(this.task.title);
-    this.taskService.addTask(this.task);
+    console.log('tamanho');
+    let size;
+    this.tasks.subscribe(function(result){
+      console.log(size = result.length)
+    });
+    
+    console.log(size);
+   // this.taskService.addTask(this.task);
   }
 
   getTasks() {
+    
     this.tasks = this.db.list('tasks').valueChanges();
+   
   }
 
 }
